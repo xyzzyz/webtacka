4351b6fa38c6301b146bc5a96342e3f1fde7811e
Adam Michalik <adam@michalik.es>
2012-07-21 15:08:32 -0700

    some websockets support in backend

--- backend/Main.hs
+++ backend/Main.hs
@@ -1,2 +1,33 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import Data.Text (Text)
+import Network.WebSockets
+import System.IO
+import System.Environment
+import Control.Monad
+import Control.Monad.Trans
+import Control.Exception
+import Control.Concurrent
+import Control.Concurrent.STM
+
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+
+type Client = Sink Hybi00
+
 main :: IO ()
-main = putStrLn "EHLO"
+main = do
+  runServer "0.0.0.0" 9160 $ application
+
+application :: Request -> WebSockets Hybi00 ()
+application rq = do
+  liftIO $ putStrLn "Ktos przylazl"
+  acceptRequest rq
+  getVersion >>= liftIO . putStrLn . ("Client version: " ++)
+  sink <- getSink
+  liftIO $ (forkIO $ handleClient sink) >> return ()
+
+handleClient sink = do
+  sendSink sink $ textData ("SIEMA CO TAM" :: Text)
+  sendSink sink $ close ("WON" :: Text)
