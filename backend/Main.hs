c89104c7df09ebb2763610194417c05aea61c90d
Adam Michalik <adam@michalik.es>
2012-07-21 15:44:27 -0700

    client/server connection handling

--- backend/Main.hs
+++ backend/Main.hs
@@ -2,6 +2,8 @@
 module Main where
 
 import Data.Text (Text)
+import Network (listenOn, PortID(PortNumber))
+import Network.Socket (accept, withSocketsDo, Socket)
 import Network.WebSockets
 import System.IO
 import System.Environment
@@ -14,20 +16,35 @@ import Control.Concurrent.STM
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 
-type Client = Sink Hybi00
+type Client = Sink Hybi10
 
 main :: IO ()
-main = do
-  runServer "0.0.0.0" 9160 $ application
+main = withSocketsDo $ do
+  servSock <- listenOn $ PortNumber 9160
+  acceptChan <- atomically newTChan
+  forkIO $ acceptLoop servSock acceptChan
+  mainLoop servSock acceptChan
 
-application :: Request -> WebSockets Hybi00 ()
+acceptLoop :: Socket -> TChan Socket -> IO ()
+acceptLoop serverSock acceptChan = do
+  (clientSock, sockAddr) <- accept serverSock
+  atomically $ writeTChan acceptChan clientSock
+  acceptLoop serverSock acceptChan
+
+mainLoop :: Socket -> TChan Socket -> IO ()
+mainLoop serverSock acceptChan = do
+  clientSock <- atomically $ readTChan acceptChan
+  forkIO $ runWithSocket clientSock application
+  mainLoop serverSock acceptChan
+
+
+application :: Request -> WebSockets Hybi10 ()
 application rq = do
   liftIO $ putStrLn "Ktos przylazl"
   acceptRequest rq
   getVersion >>= liftIO . putStrLn . ("Client version: " ++)
-  sink <- getSink
-  liftIO $ (forkIO $ handleClient sink) >> return ()
-
-handleClient sink = do
-  sendSink sink $ textData ("SIEMA CO TAM" :: Text)
-  sendSink sink $ close ("WON" :: Text)
+  send $ textData ("{\"type\": \"OK\"}" :: Text)
+  msg <- receiveData
+  liftIO $ T.putStrLn msg
+  send $ close ("WON" :: Text)
+  return ()
