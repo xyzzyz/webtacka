444ffaf4b47b39ef3263f306fd56682f244451b7
Mateusz J. Kwapich <mitrandir77@0x04.net>
2012-07-22 05:00:51 -0700

    hsv2rgb

--- frontend/js/engine.js
+++ frontend/js/engine.js
@@ -359,15 +359,15 @@ function WebGLPrepare(users) {
 			players[xplr].max_hue = ah + 180/plrcnt;
 			ah += 360/plrcnt;
 			ui_set_user_color(xplr, rgbtostring(hsv2rgb(new hsv(players[xplr].min_hue, 1, 1))), 
-					rgbtostring(hsvtorgb(new hsv(players[xplr].max_hue, 1, 1))))
+					rgbtostring(hsv2rgb(new hsv(players[xplr].max_hue, 1, 1))))
 		}
 	} else {
 		var ah=0;
 		for(xplr in players) {
 			players[xplr].min_hue = ah;
 			players[xplr].max_hue = ah;
-			ui_set_user_color(xplr, rgbtostring(hsvtorgb(new hsv(players[xplr].min_hue, 1, 1))), 
-					rgbtostring(hsvtorgb(new hsv(players[xplr].max_hue, 1, 1))))
+			ui_set_user_color(xplr, rgbtostring(hsv2rgb(new hsv(players[xplr].min_hue, 1, 1))), 
+					rgbtostring(hsv2rgb(new hsv(players[xplr].max_hue, 1, 1))))
 			ah += 360/plrcnt;
 		}
 	}
