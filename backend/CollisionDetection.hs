459971fb12c0993a6ba815092a30c9f43ca51c5d
Jaro <jaro3000@gmail.com>
2012-07-22 07:17:02 +0000

    jeszcze inna

--- backend/CollisionDetection.hs
+++ backend/CollisionDetection.hs
@@ -15,8 +15,8 @@ data Segment = Segment { first :: Point
 
 segIntersection :: Segment -> Segment -> Bool
 segIntersection (Segment (Point x1 y1) (Point x2 y2)) (Segment (Point x3 y3) (Point x4 y4)) =
-		((x1-x2)*(y3-y2)-(y1-y2)*(x3-x4))*((x1-x2)*(y4-y2)-(y1-y2)*(x4-x2)) < -0.00001 &&
-		((x1-x3)*(y4-y3)-(y1-y3)*(x4-x3))*((x2-x3)*(y4-y3)-(y2-y3)*(x4-x3)) < -0.00001 
+		((x1-x2)*(y3-y2)-(y1-y2)*(x3-x4))*((x1-x2)*(y4-y2)-(y1-y2)*(x4-x2)) < -0.1 &&
+		((x1-x3)*(y4-y3)-(y1-y3)*(x4-x3))*((x2-x3)*(y4-y3)-(y2-y3)*(x4-x3)) < -0.1 
 
 data Tree = Leaf [Segment] Rectangle | Node Tree Tree Tree Tree Rectangle deriving Show
 
