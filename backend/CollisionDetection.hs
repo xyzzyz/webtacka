9e91f7ae5feb5ffb5186e8d0a99927ec722eb868
Jaro <jaro3000@gmail.com>
2012-07-22 03:44:19 +0000

    Kolizje nie dzialaja

--- backend/CollisionDetection.hs
+++ backend/CollisionDetection.hs
@@ -1,12 +1,12 @@
+import Debug.Trace;
+
 data Rectangle = Rectangle { left :: Float
 			   , right :: Float
   			   , top :: Float
  			   , down :: Float
 			   } deriving (Show, Eq)
 
-data Point = Point { x :: Float
-		   , y :: Float
-	           } deriving (Show, Eq)
+data Point = Point Float Float deriving (Show, Eq)
 
 data Segment = Segment { first :: Point
                        , second :: Point
@@ -17,31 +17,31 @@ segIntersection (Segment (Point x1 y1) (Point x2 y2)) (Segment (Point x3 y3) (Po
 		((x1-x2)*(y3-y2)-(y1-y2)*(x3-x4))*((x1-x2)*(y4-y2)-(y1-y2)*(x4-x2))<0 &&
 		((x1-x3)*(y4-y3)-(y1-y3)*(x4-x3))*((x2-x3)*(y4-y3)-(y2-y3)*(x4-x3))<0 
 
-data Tree = Leaf [Segment] Rectangle | Node Tree Tree Tree Tree Rectangle
+data Tree = Leaf [Segment] Rectangle | Node Tree Tree Tree Tree Rectangle deriving Show
 
 segTreeIntersection :: Segment -> Tree -> Bool
 segTreeIntersection s (Leaf [] r) = False
 segTreeIntersection s (Leaf (l:ls) r) =
-   			if segRectIntersection s r then  
-				segIntersection s l || (segTreeIntersection s (Leaf ls r))
+   			if (segRectIntersection s r) then  
+				(segIntersection s l) || (segTreeIntersection s (Leaf ls r))
 			else False
 segTreeIntersection s (Node t1 t2 t3 t4 r) = 
-			if segRectIntersection s r then
-				segTreeIntersection s t1 || segTreeIntersection s t2 || segTreeIntersection s t3 || segTreeIntersection s t4 
+			if (segRectIntersection s r) then
+				(segTreeIntersection s t1) || (segTreeIntersection s t2) || (segTreeIntersection s t3) || (segTreeIntersection s t4)
 			else False
 
 
 addSegment :: Tree -> Segment -> Tree
 addSegment (Leaf l r) s = if segRectIntersection s r then 
-				(Leaf (s:l) r)
-			else (Leaf l r) 
-addSegment (Node t1 t2 t3 t4 r) s = 
+				trace "Dodalem" (Leaf (s:l) r)
+			else trace "nie dodalem" (Leaf l r) 
+addSegment (Node t1 t2 t3 t4 r) s = trace "addSegment" (
 			if segRectIntersection s r then
-				(Node (addSegment t1 s) (addSegment t2 s) (addSegment t3 s) (addSegment t4 s) r) 	
-			else (Node t1 t2 t3 t4 r)
+				(Node (addSegment t1 s) (addSegment t2 s) (addSegment t3 s) (addSegment t4 s) r)
+			else (Node t1 t2 t3 t4 r))
 
 buildTree :: Rectangle -> Tree
-buildTree r = if (right r - left r) < 0.1 then (Leaf [] r)
+buildTree r = if ((right r) - (left r)) < 0.5 then trace "Test" (Leaf [] r)
 	      else (Node (leftTop r) (rightTop r) (leftDown r) (rightDown r) r)
 
 leftTop :: Rectangle -> Tree
@@ -56,7 +56,7 @@ rightTop (Rectangle l r t d) =
 
 leftDown :: Rectangle -> Tree
 leftDown (Rectangle l r t d) = 
-	let rect = (Rectangle l ((r+1)/2) ((d+t)/2) d) 
+	let rect = (Rectangle l ((r+l)/2) ((d+t)/2) d) 
 		in buildTree rect
 
 rightDown :: Rectangle -> Tree
@@ -67,7 +67,7 @@ rightDown (Rectangle l r t d) =
 segRectIntersection :: Segment -> Rectangle -> Bool
 segRectIntersection (seg @ (Segment f s)) (rect @ (Rectangle l r t d)) =
 	if (isPointInRect f rect) then True
-	else let (s1, s2, s3, s4) = (rectToSegs rect) in (segIntersection seg s1 || segIntersection seg s2 || segIntersection seg s3 || segIntersection seg s4) 
+	else let (s1, s2, s3, s4) = (rectToSegs rect) in ((segIntersection seg s1) || (segIntersection seg s2) || (segIntersection seg s3) || (segIntersection seg s4)) 
   	
 isPointInRect :: Point -> Rectangle -> Bool
 isPointInRect (Point x y) (Rectangle l r t d) = (x>l && x<r && y<t && y>d) 
