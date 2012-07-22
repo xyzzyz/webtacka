57d30db2214e7b9c87a6e703b280418875f3de60
Mateusz J. Kwapich <mitrandir77@0x04.net>
2012-07-22 04:54:29 -0700

    bb

--- frontend/js/engine.js
+++ frontend/js/engine.js
@@ -331,7 +331,6 @@ player.prototype.addPoint = function(pnt, direction)
 }
 
 function WebGLPrepare(users) {
-  log(users);
 	players = new Array();
   $.each(users, function(id, user){
     players[user.nick] = new player();
