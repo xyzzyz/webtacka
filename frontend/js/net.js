36915da577343e17bf9f17be12d0e37558dc9ede
Mateusz J. Kwapich <mitrandir77@0x04.net>
2012-07-22 01:09:49 -0700

    juaj

--- frontend/js/net.js
+++ frontend/js/net.js
@@ -17,25 +17,37 @@ $(function(){
                         logged_in();
                     },
                     'login_err': function(e){
-                        login_err();
+                        login_err(e.data.error);
+                    },
+                    'join_err': function(e){
+                        join_err(e.data.error);
                     },
                     'room_list': function(e){
-                        update_room_list(e.data);
+                        log(e);
+                        update_room_list(e.data.rooms);
                     },
                     'joined': function(e){
-                        joined(e.data.id);
+                        joined(e.data.id, e.data.owner);
                     },
                     'room_data': function(e){
                         update_room(e.data.people);
                     },
-                    'err': function(e){
-                        fail("");
+                    'protocol_error': function(e){
+                        fail(e.data.error);
                     },
+                    'prepare': function(e){
+                      prepare_game(e.data);
+
+                    }
                 }
             });
 });
 
 function lsend(type, params){
+    if(typeof(params) == "undefined"){
+      params = {};
+    }
+
     console.log('Sending ' + type +' message - data: '+JSON.stringify(params));
     ws.send(type, params);
 }
@@ -49,8 +61,8 @@ function send_hello(nick){
 function send_get_rooms(){
     lsend('get_rooms');
 }
-function send_create_room(){
-    lsend('create_room');
+function send_create_room(capacity){
+    lsend('create_room', {'capacity':capacity});
 }
 function send_start(){
     lsend('start');
@@ -59,4 +71,3 @@ function send_start(){
 function send_join(room_id){
     lsend('join', { 'room': room_id});
 }
-
